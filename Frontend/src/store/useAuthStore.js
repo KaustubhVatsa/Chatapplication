9be67d8c5037bab14be696a919123956dc1177ce
kaustubh vatsa <kaustubh26vatsa@gmail.com>
2024-12-22 23:12:27 +0530

    attempt-03

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5002"
-    : import.meta.env.MODE || "/";
+    :"/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
