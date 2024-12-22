56b9f8fa6ca0374b36155dea8cc7fec940073d71
kaustubh vatsa <kaustubh26vatsa@gmail.com>
2024-12-22 21:49:17 +0530

    project-deployment-01

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,9 +6,11 @@ import { connectDB } from "./libs/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import { app, server } from "./libs/socket.js";
+
+import path from "path"
 //middlewares
 dotenv.config();
-
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
@@ -19,6 +21,16 @@ app.use(cors({
 //calling route
 app.use("/api/auth",authroutes)
 app.use("/api/message",messageRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+    })
+
+}
+
 server.listen(process.env.PORT ,()=>{
     console.log(`Server listening on Port :${process.env.PORT}`)
     connectDB();
